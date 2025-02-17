b89532bd20d06f71c3a82bb742bc95ef642fcd32
Jeevankumar-s <jeevenkumar2003@gmail.com>
2025-02-17 19:30:25 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// server.js
 const express = require('express');
+const cors = require('cors');  // Import the cors module
 const app = express();
 const { sequelize } = require('./models');
+
+app.use(cors());  // Enable CORS for all origins
 
 app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/outpass', require('./routes/outpassRoutes'));
 
-app.get('/helloworld',(req,res)=>{
-    res.send("Hello World")
-})
+app.get('/helloworld', (req, res) => {
+  res.send("Hello World");
+});
 
 // Sync database and start server
 sequelize.sync({ alter: true }).then(() => {
